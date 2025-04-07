846cb8a07b9251a75ee7b69055dd757dc3c0412b
Shieme Jay G. Balatero <shiemejaybalatero@email.com>
2025-04-07 23:29:15 +0800

    feat: change the whole landing page. sync: my code from borrowed laptop

--- src/views/auth/LandingPageView.vue
+++ src/views/auth/LandingPageView.vue
@@ -1,55 +1,92 @@
-<script setup>
-import { ref } from 'vue'
-</script>
+<script setup></script>
 
 <template>
-  <v-app>
-    <v-container
-      fluid
-      class="background d-flex justify-content-center align-items-center flex-column"
-    >
-      <v-row class="text-center ms-0 mb-0">
-        <v-col cols="12" md="6">
-          <img src="/public/23.png" alt="Logo" width="175px" height="175px" class="logo" />
-        </v-col>
-        <v-col cols="12">
-          <h1 class="text-black text-center fs-2 fs-md-1">
-            <b
-              >Are you a CSU student on a quest <br />
-              for the perfect place to stay?</b
-            ><br />
-            This app makes your quest quick and easy!
-          </h1>
-        </v-col>
-      </v-row>
-    </v-container>
-  </v-app>
+  <v-container fluid class="fill-height d-flex flex-column align-center justify-center bg-image">
+    <v-img src="/23.png" alt="logo" class="logodormquest mb-4" width="180" contain></v-img>
+
+    <v-row justify="center" class="text-center mb-5">
+      <v-col cols="12">
+        <h1 class="firsttext fw-bold mb-0">
+          <b> Are you a CSU Student <br />looking for a place to stay? </b>
+        </h1>
+        <h1 class="secondtext fw-light pb-5">
+          Start a <i>Quest</i> to find a place <br />to stay, anytime, anywhere.
+        </h1>
+      </v-col>
+    </v-row>
+
+    <v-row justify="center" class="text-center">
+      <v-col cols="12">
+        <RouterLink to="/login">
+          <v-btn class="custom-btn mb-5">Login</v-btn>
+        </RouterLink>
+        <br />
+        <RouterLink to="/register">
+          <v-btn class="custom-btn btntwo text-white">Create an account</v-btn>
+        </RouterLink>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 <style scoped>
-.background {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  background: url('/public/landing1.jpg') no-repeat center;
+/* Background styling */
+.bg-image {
+  background-image: url('/landing4.png');
   background-size: cover;
-  background-attachment: fixed;
-}
-.background::before {
-  content: '';
-  position: absolute;
-  top: 0;
-  left: 0;
-  width: 100%;
+  background-position: center;
   height: 100%;
-  background: rgba(220, 226, 219, 0.3); /* Adjust transparency here */
 }
 
-.logo {
-  margin-top: 0;
-  position: absolute;
-  top: 70px;
-  left: 49.2%;
-  transform: translateX(-50%);
+.fill-height {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  padding: 0;
+  margin: 0;
+}
+
+.v-row {
+  margin-bottom: 0;
+}
+
+/* Button styles */
+.custom-btn {
+  width: 15em;
+  font-size: 16px;
+  border-radius: 0 2rem;
+  background-color: #ffba00 !important;
+  color: black !important;
+  text-transform: none;
+}
+
+.custom-btn:hover {
+  background-color: #ffba00 !important;
+  opacity: 1;
+  border: 2px solid #0c3b2e;
+}
+
+.btntwo {
+  background-color: #0c3b2e !important;
+  border-radius: 0 2rem;
+  width: 15em;
+  text-transform: none;
+}
+
+.btntwo:hover {
+  background-color: #0c3b2e !important;
+  opacity: 1;
+  border: 2px solid #ffba00;
+}
+
+.firsttext {
+  font-size: 18px;
+  color: #0c3b2e;
+}
+
+.secondtext {
+  font-size: 17px;
+  color: #0c3b2e;
 }
 </style>
