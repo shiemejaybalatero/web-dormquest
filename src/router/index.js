59ab6eab74d633d26ec36fe47e9ba798fe0b275d
Zyeke Gador <gadorzyekeaira@gmail.com>
2025-04-17 00:56:10 +0800

    Fix router path for RatingsView and resolve merge conflict

--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,13 @@ import DashBoardView from '@/views/auth/DashBoardView.vue'
 import GoogleMapView from '@/views/auth/GoogleMapView.vue'
 import ProfileView from '@/views/auth/ProfileView.vue'
 import TgbgBoardingHouseView from '@/views/auth/TgbgBoardingHouseView.vue'
+import AmplayoBoardingHouseView from '@/views/auth/AmplayoBoardingHouseView.vue'
+import BlissfulBoardingHouseView from '@/views/auth/BlissfulBoardingHouseView.vue'
+import BlueBoardingHouseView from '@/views/auth/BlueBoardingHouseView.vue'
+import ChelseaBoardingHouseView from '@/views/auth/ChelseaBoardingHouseView.vue'
+import KarmoBoardingHouseView from '@/views/auth/KarmoBoardingHouseView.vue'
+import LicayanBoardingHouseView from '@/views/auth/LicayanBoardingHouseView.vue'
+import MagduraBoardingHouseView from '@/views/auth/MagduraBoardingHouseView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -25,31 +32,61 @@ const router = createRouter({
       name: 'register',
       component: RegisterView,
     },
-
     {
       path: '/dashboard',
       name: 'dashboard',
       component: DashBoardView,
     },
-
     {
       path: '/map',
       name: 'map',
       component: GoogleMapView,
     },
-
     {
       path: '/profile',
       name: 'profile',
       component: ProfileView,
     },
-
     {
       path: '/profile/ratings',
       name: 'ratings',
       component: () => import('@/views/auth/RatingsView.vue'),
     },
-
+    {
+      path: '/amplayoboardinghousedetails',
+      name: 'amplayoboardinghousedetails',
+      component: AmplayoBoardingHouseView,
+    },
+    {
+      path: '/blissfulboardinghousedetails',
+      name: 'blissfulboardinghousedetails',
+      component: BlissfulBoardingHouseView,
+    },
+    {
+      path: '/blueboardinghousedetails',
+      name: 'blueboardinghousedetails',
+      component: BlueBoardingHouseView,
+    },
+    {
+      path: '/chelseaboardinghousedetails',
+      name: 'chelseaboardinghousedetails',
+      component: ChelseaBoardingHouseView,
+    },
+    {
+      path: '/karmoboardinghousedetails',
+      name: 'karmoboardinghousedetails',
+      component: KarmoBoardingHouseView,
+    },
+    {
+      path: '/licayanboardinghousedetails',
+      name: 'licayanboardinghousedetails',
+      component: LicayanBoardingHouseView,
+    },
+    {
+      path: '/magduraboardinghousedetails',
+      name: 'magduraboardinghousedetails',
+      component: MagduraBoardingHouseView,
+    },
     {
       path: '/tgbgboardinghousedetails',
       name: 'tgbgboardinghousedetails',
