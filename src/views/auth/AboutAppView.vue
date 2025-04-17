8cd37828f9a5110865cf71a00a46379db3ade39b
zyekegador <gadorzyekeaira@gmail.com>
2025-04-18 00:29:00 +0800

    about app info

--- src/views/auth/AboutAppView.vue
+++ src/views/auth/AboutAppView.vue
@@ -8,10 +8,10 @@ const route = useRoute()
 <template>
   <AppLayout>
     <template #content>
-      <v-row>
+      <v-row class="pa-6">
         <!-- Sidebar -->
         <v-col cols="12" md="3">
-          <v-list dense nav class="sidebar pa-4">
+          <v-list dense nav class="sidebar">
             <v-list-item class="mt-3 mb-2">
               <div class="d-flex align-center pl-2">
                 <v-icon color="#0c3b2e" class="mr-2">mdi-account</v-icon>
@@ -19,34 +19,34 @@ const route = useRoute()
               </div>
             </v-list-item>
 
-            <router-link to="/ratings" style="text-decoration: none">
-              <v-list-item :class="{ selected: route.path === '/ratings' }" class="mb-2">
+            <router-link to="/profile/ratings" style="text-decoration: none; color: inherit">
+              <v-list-item :class="{ selected: route.path === '/profile/ratings' }" class="mb-2">
                 <div class="d-flex align-center pl-2">
-                  <v-icon class="mr-2">mdi-star</v-icon>
+                  <v-icon class="mr-2" color="#0c3b2e">mdi-star</v-icon>
                   <span class="font-weight-bold text-body-1">Ratings</span>
                 </div>
               </v-list-item>
             </router-link>
 
-            <router-link to="/about" style="text-decoration: none">
-              <v-list-item :class="{ selected: route.path === '/about' }" class="mb-2">
+            <router-link to="profile/about" style="text-decoration: none; color: inherit">
+              <v-list-item :class="{ selected: route.path === '/profile/about' }" class="mb-2">
                 <div class="d-flex align-center pl-2">
-                  <v-icon class="mr-2">mdi-information</v-icon>
+                  <v-icon class="mr-2" color="#0c3b2e">mdi-information</v-icon>
                   <span class="font-weight-bold text-body-1">About app</span>
                 </div>
               </v-list-item>
             </router-link>
 
-            <v-list-item @click="console.log('Logging out...')">
+            <v-list-item>
               <div class="d-flex align-center pl-2">
-                <v-icon class="mr-2">mdi-logout</v-icon>
+                <v-icon class="mr-2" color="#0c3b2e">mdi-logout</v-icon>
                 <span>Log out</span>
               </div>
             </v-list-item>
           </v-list>
         </v-col>
 
-        <!-- About App Section -->
+        <!-- Main Content -->
         <v-col cols="12" md="9">
           <div class="about-section pa-6">
             <div class="d-flex justify-space-between align-center mb-6">
@@ -88,6 +88,7 @@ const route = useRoute()
   min-height: 40vh;
   background: linear-gradient(180deg, #dbead3, #6d9773);
   border-radius: 16px;
+  padding: 24px;
 }
 
 .selected {
