ced2ad98ac22280d6653e1bb68877d8ee6dc184a
Antonio Granza <granzaantonio320@gmail.com>
2025-04-23 21:47:50 +0800

    feat:added .env

--- src/views/auth/AmplayoBoardingHouseView.vue
+++ src/views/auth/AmplayoBoardingHouseView.vue
@@ -122,7 +122,7 @@ const images = [
       <v-col cols="12" md="6">
         <v-row dense class="mb-4">
           <v-col v-for="(img, index) in images" :key="index" cols="6" md="6">
-            <v-img :src="img" height="130" class="rounded" cover />
+            <v-img :src="img" height="170" class="rounded" cover />
           </v-col>
         </v-row>
 
