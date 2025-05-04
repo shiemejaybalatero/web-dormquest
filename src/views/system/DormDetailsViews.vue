fd20c8af768f62cfccf29bd8a6c0e10e679dddc2
Antonio Granza <granzaantonio320@gmail.com>
2025-05-04 20:57:11 +0800

    fix

--- src/views/system/DormDetailsViews.vue
+++ src/views/system/DormDetailsViews.vue
@@ -5,7 +5,6 @@ import { useBoardingHouseStore } from '@/stores/boardingHouse'
 import { useTheme } from 'vuetify' // Import useTheme to access theme state
 import AppLayout from '@/components/layout/AppLayout.vue'
 import DormitoryReview from '@/components/system/DormitoryReview.vue'
-import { dormImageMap } from '@/stores/DormImages'
 import { supabase } from '@/utils/supabase'
 
 const route = useRoute()
@@ -41,6 +40,61 @@ const displayRating = computed(() => {
   const rating = ratingStats.value.average || (dormDetails.value?.rating ?? 0)
   return rating.toFixed(1)
 })
+
+const dormImageMap = {
+  1: {
+    main: '/Amplayo/amplayomain.png',
+    gallery: [
+      '/Amplayo/amplayo.png',
+      '/Amplayo/amplayo1.jpg',
+      '/Amplayo/amplayo2.png',
+      '/Amplayo/amplayo3.png',
+    ],
+  },
+  2: {
+    main: '/BlueHeaven/bluemain.png',
+    gallery: ['/BlueHeaven/blue.jpg', '/BlueHeaven/blue1.jpg', '/BlueHeaven/blue2.jpg'],
+  },
+  3: {
+    main: '/Blissful/blissfulmain.png',
+    gallery: ['/Blissful/blissful.jpg', '/Blissful/blissful1.jpg', '/Blissful/blissful2.jpg'],
+  },
+  4: {
+    main: '/Licayan/licayanmain.png',
+    gallery: [
+      '/Licayan/licayan.png',
+      '/Licayan/licayan1.png',
+      '/Licayan/licayan2.jpg',
+      '/Licayan/licayan3.jpg',
+    ],
+  },
+  5: {
+    main: '/Chelsea/chelseamain.jpg',
+    gallery: [
+      '/Chelsea/chelsea.jpg',
+      '/Chelsea/chelsea1.jpg',
+      '/Chelsea/chelsea2.jpg',
+      '/Chelsea/chelsea3.jpg',
+    ],
+  },
+  6: {
+    main: '/TGBG/tgbgmain.png',
+    gallery: ['/TGBG/tgbg.png', '/TGBG/tgbg1.png', '/TGBG/tgbg2.png', '/TGBG/tgbg3.png'],
+  },
+  7: {
+    main: '/Magdura/magduramain.png',
+    gallery: [
+      '/Magdura/magdura.png',
+      '/Magdura/magdura1.png',
+      '/Magdura/magdura2.png',
+      '/Magdur3.png',
+    ],
+  },
+  8: {
+    main: '/Karmo/karmomain.jpg',
+    gallery: ['/Karmo/karmo.jpg', '/Karmo/karmo1.jpg', '/Karmo/karmo2.jpg'],
+  },
+}
 
 const defaultMainImage = '/Default/default.jpg'
 const defaultGallery = ['/Default/default.jpg']
